0eb10e586606ade12cf702adf6cebe7a39f1d511
Richard Marks <ccpsceo@gmail.com>
2009-03-20 10:36:13 -0500

    reworked the simulation controls and hud added range of sight and fatigue meters to display

--- main.cpp
+++ main.cpp
@@ -18,6 +18,15 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
+/*
+ 
+	I need to work in some kind of factor to slow down the commandos..right now they calculate their target vector and make a bee-line for it
+	I need some kind of alert-zone for a targeted commando to "know" when he is about to get his ass whooped... XD
+
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+
 static volatile bool mainThreadIsRunning = true;
 static volatile int allegroTimerSpeedCounter = 0;
 static void AllegroCloseButtonHandler(){mainThreadIsRunning = false;}END_OF_FUNCTION(AllegroCloseButtonHandler)
@@ -32,6 +41,37 @@ enum AIActionName
 	AI_DoNothing
 };
 
+enum AISituationStateName
+{
+	/// the combat entity is idle, doing nothing at this time
+	AI_Idle,
+	
+	/// the combat entity is patrolling the area
+	AI_Patrolling,
+	
+	/// the combat entity is standing guard at his post and will not move unless provoked
+	AI_Guarding,
+	
+	/// the combat entity is attacking its target
+	AI_Attacking,
+	
+	/// the combat entity is chasing after its target
+	AI_Chasing,
+	
+	/// the combat entity is evading; will run if any other entities get close
+	AI_Evading,
+	
+	// the combat entity will move to a random position, memorizing the number of entities within his FOV
+	// and return to his original position, alerting his team members with his report
+	AI_Scouting,
+	
+	// the combat entity is fatigued, and needs to rest until his fatigue is gone
+	AI_Resting
+};
+/*
+	if the entity is idle and 
+*/
+
 ////////////////////////////////////////////////////////////////////////////////
 
 /*
@@ -45,6 +85,10 @@ enum AIActionName
 	health_ is the amount of damage that the combat entity can take before it is killed
 	
 	shield_ is the amount of damage that the combat entity can take before its health will be touched
+	
+	fatigue_ is the number of times the combat entity can move before needing to rest
+	
+	rangeOfSight_ is the distance from the combat entity that it can "see"
 	
 */
 class CombatEntityProperties
@@ -61,35 +105,44 @@ public:
 	{
 		this->Zero();
 	}
-	
+
 	// setters
 	void SetCombatExperience(int amount = 0) 		{ combatExperience_ = amount; }
 	void SetStrength(int amount = 0) 				{ strength_ = amount; }
 	void SetHealth(int amount = 0) 					{ health_ = amount; }
 	void SetShield(int amount = 0) 					{ shield_ = amount; }
+	void SetFatigue(int amount = 0) 				{ fatigue_ = amount; }
+	void SetRangeOfSight(int amount = 0) 			{ rangeOfSight_ = amount; }
 	
 	// incrementers
 	void IncreaseCombatExperience(int amount = 1) 	{ combatExperience_ += amount; }
 	void IncreaseStrength(int amount = 1) 			{ strength_ += amount; }
 	void IncreaseHealth(int amount = 1) 			{ health_ += amount; }
 	void IncreaseShield(int amount = 1) 			{ shield_ += amount; }
+	void IncreaseFatigue(int amount = 1) 			{ fatigue_ += amount; }
+	void IncreaseRangeOfSight(int amount = 1) 		{ rangeOfSight_ += amount; }
 	
 	// decrementers
 	void DecreaseCombatExperience(int amount = 1) 	{ combatExperience_ -= amount; }
 	void DecreaseStrength(int amount = 1) 			{ strength_ -= amount; }
 	void DecreaseHealth(int amount = 1) 			{ health_ -= amount; }
 	void DecreaseShield(int amount = 1) 			{ shield_ -= amount; }
+	void DecreaseFatigue(int amount = 1) 			{ fatigue_ -= amount; }
+	void DecreaseRangeOfSight(int amount = 1) 		{ rangeOfSight_ -= amount; }
 	
 	// getters
 	int GetCombatExperience() const 				{ return combatExperience_; }
 	int GetStrength() const 						{ return strength_; }
 	int GetHealth() const 							{ return health_; }
 	int GetShield() const 							{ return shield_; }
+	int GetFatigue() const							{ return fatigue_; }
+	int GetRangeOfSight() const						{ return rangeOfSight_; }
 	
 	// adds the inheritable properties of the parent to this entity
 	void InheritProperties(const CombatEntityProperties& parent)
 	{
 		this->IncreaseStrength(parent.strength_);
+		this->IncreaseRangeOfSight(parent.rangeOfSight_);
 	}
 	
 	// clones the properties of the source entity to this entity
@@ -99,6 +152,8 @@ public:
 		this->SetStrength(source.strength_);
 		this->SetHealth(source.health_);
 		this->SetShield(source.shield_);
+		this->SetFatigue(source.fatigue_);
+		this->SetRangeOfSight(source.rangeOfSight_);
 	}
 	
 	// clears all properties to the lowest values
@@ -108,6 +163,8 @@ public:
 		this->SetStrength();
 		this->SetHealth();
 		this->SetShield();
+		this->SetFatigue();
+		this->SetRangeOfSight();
 	}
 	
 private:
@@ -115,6 +172,8 @@ private:
 	int strength_;
 	int health_;
 	int shield_;
+	int fatigue_;
+	int rangeOfSight_;
 }; // end class
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -458,14 +517,18 @@ void Commando::Update()
 
 void Commando::Render(BITMAP* target)
 {
+	int wOver2 = width_ / 2;
+	int hOver2 = height_ / 2;
+	
 	if (AI_DoConvergence == ai_ && !reachedAITarget_)
 	{
-		int wOver2 = width_ / 2;
-		int hOver2 = height_ / 2;
 		line(target, x_ + wOver2, y_ + hOver2, aiTargetX_ + wOver2, aiTargetY_ + hOver2, makecol(64, 0, 0));
 	}
 	
-	rectfill(target, x_, y_, x_ + width_, y_ + height_, color_);	
+	rectfill(target, x_, y_, x_ + width_, y_ + height_, color_);
+	
+	circle(target, x_ + wOver2, y_ + hOver2, properties_.GetRangeOfSight(), makecol(0, 64, 0));	
+	circle(target, x_ + wOver2, y_ + hOver2, properties_.GetFatigue(), makecol(64, 0, 0));	
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -509,17 +572,29 @@ public:
 	void Destroy();
 	
 private:
-	CombatEntityList combatEntities_;
+	// simulator "engine" members
+	bool isSimulationRunning_;
 	bool lmbIsDown_;
 	bool rmbIsDown_;
+	bool keyDown_[KEY_MAX];
+	
+private:
+	// simulation object members
+	CombatEntityList combatEntities_;
+	
 }; // end class
 
 ////////////////////////////////////////////////////////////////////////////////
 
 Simulator::Simulator() :
+	isSimulationRunning_(false),
 	lmbIsDown_(false),
 	rmbIsDown_(false)
 {
+	for (int index = 0; index < KEY_MAX; index++)
+	{
+		keyDown_[index] = false;
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -559,6 +634,8 @@ void Simulator::Update()
 			rollOfTheDice.SetStrength(10 + rand() % (20 - 10));
 			rollOfTheDice.SetHealth(100 + rand() % (200 - 100));
 			rollOfTheDice.SetShield(100 + rand() % (200 - 100));
+			rollOfTheDice.SetFatigue(32 + rand() % (96 - 32));
+			rollOfTheDice.SetRangeOfSight((SCREEN_H / 8) + rand() % ((SCREEN_H / 5) - (SCREEN_H / 8)));
 			nextCommando->GetProperties().Clone(rollOfTheDice);
 			combatEntities_.Push(nextCommando);
 			
@@ -577,48 +654,100 @@ void Simulator::Update()
 	{
 		if (rmbIsDown_)
 		{
-			// click
+			rmbIsDown_ = false;
+			// do something when the right mouse button is clicked
+		}
+	}
+	
+	// pick convergence simulation
+	if (key[KEY_1])
+	{
+		if (!keyDown_[KEY_1])
+		{
+			keyDown_[KEY_1] = true;
+		}
+	}
+	else
+	{
+		if (keyDown_[KEY_1])
+		{
+			keyDown_[KEY_1] = false;
 			
-			// pick a random commando to target
-			size_t commando = rand() % static_cast<int>(combatEntities_.Size() - 1);
-			combatEntities_[commando]->Target();
-			
-			// alert all other commandos about the target
-			size_t count = combatEntities_.Size();
-			for (size_t index = 0; index < count; index++)
+			// if the simulation is running, then we cannot interrupt it
+			if (!isSimulationRunning_)
 			{
-				if (index == commando)
+				// get the number of commandos
+				size_t commandoCount = combatEntities_.Size();
+				
+				// pick a random commando to target
+				size_t targetCommandoIndex = rand() % static_cast<int>(commandoCount - 1);
+				
+				// we tell all the commandos to enter the convergence mode and its target
+				for (size_t index = 0; index < commandoCount; index++)
 				{
-					continue;
+					// we skip over the target commando
+					if (targetCommandoIndex == index)
+					{
+						continue;
+					}
+					
+					// enter convergence mode
+					combatEntities_[index]->SetAIAction(AI_DoConvergence);
+					
+					// set the target of the commando
+					combatEntities_[index]->SetTargetEntity(combatEntities_[targetCommandoIndex]);
 				}
 				
-				combatEntities_[index]->SetTargetEntity(combatEntities_[commando]);
-				combatEntities_[index]->SetAIAction(AI_DoConvergence);
-			}
+				// tell the target commando that its a target
+				combatEntities_[targetCommandoIndex]->Target();
 			
-			rmbIsDown_ = false;
+			}
+		}
+	}
+	
+	// toggle simulation runtime
+	if (key[KEY_R])
+	{
+		if (!keyDown_[KEY_R])
+		{
+			keyDown_[KEY_R] = true;
+		}
+	}
+	else
+	{
+		if (keyDown_[KEY_R])
+		{
+			keyDown_[KEY_R] = false;
+			
+			isSimulationRunning_ = !isSimulationRunning_;
 		}
 	}
 	
 	
-
-
-	size_t count = combatEntities_.Size();
-	for (size_t index = 0; index < count; index++)
+	
+	// only update the simulation if we are running
+	if (isSimulationRunning_)
 	{
-		CombatEntity* entity = combatEntities_[index];
-		if (entity)
+		size_t commandoCount = combatEntities_.Size();
+		for (size_t index = 0; index < commandoCount; index++)
 		{
-			// is this entity dead?
-			if (entity->IsNeutralized())
+			CombatEntity* entity = combatEntities_[index];
+			if (entity)
 			{
-				// remove the dead entity from the field
-				combatEntities_.Erase(index);
-				continue;
-			}
+				// is this entity dead?
+				if (entity->IsNeutralized())
+				{
+					// remove the dead entity from the field
+					combatEntities_.Erase(index);
+					
+					// update the count
+					commandoCount = combatEntities_.Size();
+					continue;
+				}
 			
-			// update the entity
-			entity->Update();
+				// update the entity
+				entity->Update();
+			}
 		}
 	}
 }
@@ -627,29 +756,65 @@ void Simulator::Update()
 
 void Simulator::Render(BITMAP* target)
 {
-	int hudX = 4;
-	int hudY = 4;
+	static int hudOriginX = 4;
+	static int hudOriginY = 4;
+	int hudX = hudOriginX;
+	int hudY = hudOriginY;
 	int txtH = text_height(font);
-	int txtC = makecol(255, 255, 255);
+	int txtC = makecol(192, 192, 192);
 	
-	size_t count = combatEntities_.Size();
-	for (size_t index = 0; index < count; index++)
+	// get the number of commandos
+	size_t commandoCount = combatEntities_.Size();
+	
+	// if there are more than 0
+	if (commandoCount)
 	{
-		CombatEntity* commando = combatEntities_[index];
-		AIActionName ai = commando->GetAIAction();
+		// for each commando
+		for (size_t index = 0; index < commandoCount; index++)
+		{
+			// grab a pointer to the commando
+			CombatEntity* commando = combatEntities_[index];
+			
+			// grab a pointer to the target commando
+			CombatEntity* targetCommando = commando->GetTargetEntity();
+			
+			// get the ai action of the commando
+			AIActionName ai = commando->GetAIAction();
+			
+			// get the properties of the commando
+			CombatEntityProperties& properties = commando->GetProperties();
 		
-		CombatEntityProperties& properties = commando->GetProperties();
-		
-		textprintf_ex(target, font, hudX, hudY + (txtH * index) + (2 * index), txtC, -1,
-			"Commando %3d : Pos(%4d, %4d) Orders: %s [HP: %4d SH: %4d STR: %4d XP: %d]", 
-			static_cast<int>(index) + 1, commando->GetX(), commando->GetY(),
-			(AI_DoNothing == ai) ? "Do Nothing" : "Converge on Target",
-			properties.GetHealth(), properties.GetShield(), properties.GetStrength(), properties.GetCombatExperience());
-		
-		combatEntities_[index]->Render(target);
+			
+			// create the hud information
+			char hudLines[4][0x80];
+			sprintf(hudLines[0], "Commando %3d @ Pos(%4d, %4d)", static_cast<int>(index) + 1, commando->GetX(), commando->GetY());
+			sprintf(hudLines[1], "AI Action: %s", (AI_DoNothing == ai) ? "Do Nothing" : (AI_DoConvergence) ? "Converge on Target" : "Take Evasive Action");
+			sprintf(hudLines[2], "HP: %4d    SH: %4d    STR: %4d    XP: %6d", properties.GetHealth(), properties.GetShield(), properties.GetStrength(), properties.GetCombatExperience());
+			sprintf(hudLines[3], "Has Target? %s  Pos(%4d, %4d)", (targetCommando) ? "YES" : "NO", (targetCommando) ? targetCommando->GetX() : 0, (targetCommando)?targetCommando->GetY() : 0);
+			//sprintf(hudLines[4], "AI State: %s");
+			
+			// render the commando
+			combatEntities_[index]->Render(target);
+
+			// render the hud information
+			textprintf_ex(target, font, hudX, hudY, txtC, -1, "%s", hudLines[0]);
+			textprintf_ex(target, font, hudX, hudY + (txtH + 2), txtC, -1, "%s", hudLines[1]);
+			textprintf_ex(target, font, hudX, hudY + ((txtH * 2) + 4), txtC, -1, "%s", hudLines[2]);
+			textprintf_ex(target, font, hudX, hudY + ((txtH * 3) + 6), txtC, -1, "%s", hudLines[3]);
+			
+			// update the hude info position
+			hudY += (txtH * 5) + 10;
+			/*
+			textprintf_ex(target, font, hudX, hudY + (txtH * index) + (2 * index), txtC, -1,
+				"Commando %3d : Pos(%4d, %4d) Orders: %s [HP: %4d SH: %4d STR: %4d XP: %d]", 
+				static_cast<int>(index) + 1, commando->GetX(), commando->GetY(),
+				(AI_DoNothing == ai) ? "Do Nothing" : "Converge on Target",
+				properties.GetHealth(), properties.GetShield(), properties.GetStrength(), properties.GetCombatExperience());
+			*/
+		}
 	}
 	
-	textprintf_ex(target, font, hudX, target->h - (hudY + txtH * 2), txtC, -1, "Commandos: %d", static_cast<int>(count));
+	textprintf_ex(target, font, hudOriginX, target->h - txtH, txtC, -1, "Commandos: %d", static_cast<int>(commandoCount));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -668,7 +833,7 @@ int main(int argc, char* argv[])
 	install_timer();
 	install_mouse();
 	set_color_depth(16);
-	set_gfx_mode(GFX_AUTODETECT_WINDOWED, 800, 600, 0, 0);
+	set_gfx_mode(GFX_AUTODETECT_WINDOWED, 1024, 800, 0, 0);
 	set_window_title("AI Survival v0.2 - Developed by Richard Marks <ccpsceo@gmail.com>");
 	BITMAP* bb = create_bitmap(SCREEN_W, SCREEN_H);
 	LOCK_FUNCTION(AllegroCloseButtonHandler);
